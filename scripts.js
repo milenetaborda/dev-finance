a14eb9be2d86526dde3109276e524b857e76f3c4
Milene Taborda <milenetaborda1@outlook.com>
2021-02-19 13:58:34 -0300

    feat: function remove item by index

--- scripts.js
+++ scripts.js
@@ -68,12 +68,13 @@ const DOM = {
 
   addTransaction(transaction, index){
     const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index
 
     DOM.transactionsContainer.appendChild(tr)
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
     const amount = Utils.formatCurrency(transaction.amount)
@@ -84,7 +85,7 @@ const DOM = {
       <td class=${CSSclass}>${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-          <img src="./assets/minus.svg" alt="Remover transação">
+          <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
       </td>
     `
     return html
@@ -197,8 +198,8 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach(transaction => {
-      DOM.addTransaction(transaction)
+    Transaction.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index)
     })
 
     DOM.updateBalance()
